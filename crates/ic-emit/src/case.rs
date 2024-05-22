62778cfa1170601cf6129a9dc33c342748f5f32f
Idar Carlsen <idar.carlsen@kongsberg.com>
2024-05-22 20:59:58 +0200

    Case-conversion function in Rust

---
+++ crates/ic-emit/src/case.rs
@@ -0,0 +1,214 @@
+// Copyright 2024 KONGSBERG
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+pub enum Case {
+    Snake,
+    Camel,
+    Pascal,
+}
+
+struct Converter {
+    first: bool,
+    case: Case,
+}
+
+fn is_delim(c: char) -> bool {
+    c.is_whitespace() || c.is_ascii_punctuation()
+}
+
+impl Converter {
+    fn append(&mut self, word: &str, buffer: &mut String) {
+        if !word.is_empty() {
+            match self.case {
+                Case::Snake => self.to_snake(word, buffer),
+                Case::Pascal => self.to_pascal(word, buffer),
+                Case::Camel => self.to_camel(word, buffer),
+            }
+        }
+        self.first = false;
+    }
+
+    fn to_snake(&self, word: &str, buffer: &mut String) {
+        if !self.first {
+            buffer.push('_');
+        }
+        *buffer += &word.to_lowercase();
+    }
+
+    fn to_pascal(&self, word: &str, buffer: &mut String) {
+        let mut iter = word.chars();
+        if let Some(c) = iter.next() {
+            buffer.extend(c.to_uppercase());
+        }
+        buffer.extend(iter.flat_map(char::to_lowercase));
+    }
+
+    fn to_camel(&self, word: &str, buffer: &mut String) {
+        let mut iter = word.chars();
+        if let Some(c) = iter.next() {
+            if self.first {
+                buffer.extend(c.to_lowercase());
+            } else {
+                buffer.extend(c.to_uppercase());
+            }
+        }
+        buffer.extend(iter.flat_map(char::to_lowercase));
+    }
+
+    fn convert(mut self, input: &str) -> String {
+        let mut start = 0;
+        let mut was_upper = false;
+        let mut iter = input.chars().enumerate().peekable();
+        let mut buffer = String::with_capacity(input.len() + 5);
+
+        while let Some((i, c)) = iter.next() {
+            if is_delim(c) {
+                if i == start {
+                    start += 1;
+                }
+                continue;
+            }
+
+            if let Some((_, peek)) = iter.peek() {
+                let len = i - start;
+
+                if is_delim(*peek) || (c.is_lowercase() && peek.is_uppercase()) {
+                    self.append(&input[start..start + len + 1], &mut buffer);
+                    start = i + 1;
+                } else if was_upper && c.is_uppercase() && peek.is_lowercase() {
+                    self.append(&input[start..start + len], &mut buffer);
+                    start = i;
+                }
+            } else {
+                self.append(&input[start..], &mut buffer);
+            }
+            was_upper = c.is_uppercase();
+        }
+        buffer
+    }
+}
+
+/// Converts the given string to the specified capitalization.
+///
+/// Whitespace and ASCII punctuation characters are treated as delimiters
+/// between words. Multiple consecutive delimiters will be stripped. For
+/// example, "foo bar...baz" would be converted to "fooBarBaz" in `camelCase`.
+pub fn convert<A: AsRef<str>>(input: A, case: Case) -> String {
+    let state = Converter { first: true, case };
+    state.convert(input.as_ref())
+}
+
+/// Converts the given string to `snake_case`. See [`convert`] for more
+/// information.
+///
+/// # Example
+///
+/// ```rust
+/// # use ic_emit::case::snake_case;
+///
+/// let converted = snake_case("FooBarBaz3");
+/// assert_eq!(converted, "foo_bar_baz3");
+/// ````
+pub fn snake_case<A: AsRef<str>>(input: A) -> String {
+    convert(input, Case::Snake)
+}
+
+/// Converts the given string to `camelCase`.See [`convert`] for more
+/// information.
+///
+/// # Example
+///
+/// ```rust
+/// # use ic_emit::case::camel_case;
+///
+/// let converted = camel_case("foo_Bar_baz3");
+/// assert_eq!(converted, "fooBarBaz3");
+/// ````
+pub fn camel_case<A: AsRef<str>>(input: A) -> String {
+    convert(input, Case::Camel)
+}
+
+/// Converts the given string to `PascalCase`.See [`convert`] for more
+/// information.
+///
+/// # Example
+///
+/// ```rust
+/// # use ic_emit::case::pascal_case;
+///
+/// let converted = pascal_case("foo_Bar_baz3");
+/// assert_eq!(converted, "FooBarBaz3");
+/// ````
+pub fn pascal_case<A: AsRef<str>>(input: A) -> String {
+    convert(input, Case::Pascal)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn single_char_suffix_snake() {
+        // snake
+        assert_eq!(snake_case("suffix_t"), "suffix_t");
+        assert_eq!(snake_case("suffix_1"), "suffix_1");
+        assert_eq!(snake_case("suffix__"), "suffix");
+        assert_eq!(snake_case("suffix_1_"), "suffix_1");
+        assert_eq!(snake_case("abc_t_def"), "abc_t_def");
+        assert_eq!(snake_case("FooBarBaz"), "foo_bar_baz");
+        assert_eq!(snake_case("Foo1bar"), "foo1bar");
+        assert_eq!(snake_case("foo bar.baz"), "foo_bar_baz");
+
+        // pascal
+        assert_eq!(pascal_case("suffix_t"), "SuffixT");
+        assert_eq!(pascal_case("suffix_1"), "Suffix1");
+        assert_eq!(pascal_case("suffix__"), "Suffix");
+        assert_eq!(pascal_case("suffix_1_"), "Suffix1");
+        assert_eq!(pascal_case("abc_t_def"), "AbcTDef");
+
+        // camel
+        assert_eq!(camel_case("suffix_t"), "suffixT");
+        assert_eq!(camel_case("suffix_1"), "suffix1");
+        assert_eq!(camel_case("suffix__"), "suffix");
+        assert_eq!(camel_case("suffix_1_"), "suffix1");
+        assert_eq!(camel_case("abc_t_def"), "abcTDef");
+    }
+
+    #[test]
+    fn multiple_upper() {
+        assert_eq!(pascal_case("JSONParser"), "JsonParser");
+        assert_eq!(snake_case("IDLType"), "idl_type");
+        assert_eq!(snake_case("PROTO3Buffer"), "proto3buffer");
+    }
+
+    #[test]
+    fn single_underscore() {
+        assert_eq!(snake_case("P_Arbitration_AU_PSM"), "p_arbitration_au_psm");
+        assert_eq!(pascal_case("P_Arbitration_AU_PSM"), "PArbitrationAuPsm");
+        assert_eq!(camel_case("P_Arbitration_AU_PSM"), "pArbitrationAuPsm");
+    }
+}

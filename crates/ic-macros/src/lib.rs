010d4e37908d5102b4e0fb7a1eaff4212ed8e877
Idar Carlsen <idar.carlsen@kongsberg.com>
2024-06-11 11:02:47 +0200

    Macros for deriving string representation of enums and iterating over variants

---
+++ crates/ic-macros/src/lib.rs
@@ -0,0 +1,118 @@
+// Copyright 2024 KONGSBERG
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
+
+fn enum_iter(data: &Data) -> proc_macro2::TokenStream {
+    let mut variants = proc_macro2::TokenStream::new();
+
+    if let syn::Data::Enum(ref value) = data {
+        for (index, variant) in value.variants.iter().enumerate() {
+            let ident = &variant.ident;
+
+            variants.extend(quote! {
+                #index => Some(Self::#ident),
+            })
+        }
+    }
+    variants
+}
+
+fn enum_string(data: &Data) -> proc_macro2::TokenStream {
+    let mut variants = proc_macro2::TokenStream::new();
+
+    if let syn::Data::Enum(ref value) = data {
+        for variant in &value.variants {
+            let ident = &variant.ident;
+            let params = match &variant.fields {
+                syn::Fields::Unit => quote! {},
+                syn::Fields::Unnamed(_) => quote! { (..) },
+                syn::Fields::Named(_) => quote! { { .. } },
+            };
+
+            variants.extend(quote! {
+                Self::#ident #params => stringify!(#ident).to_string(),
+            });
+        }
+    }
+    variants
+}
+
+#[proc_macro_derive(EnumIter)]
+pub fn derive_enum_iter(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let variants = enum_iter(&input.data);
+
+    let expanded = quote! {
+        impl #impl_generics #name #ty_generics
+        #where_clause
+        {
+            pub fn iter() -> impl ::std::iter::Iterator<Item = Self> {
+                let mut state = 0;
+
+                ::std::iter::from_fn(move || {
+                    let var = match state {
+                        #variants
+                        _ => None,
+                    };
+                    state += 1;
+                    var
+                })
+            }
+        }
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(ToString)]
+pub fn derive_to_string(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let variants = enum_string(&input.data);
+
+    let expanded = quote! {
+        impl #impl_generics ::std::string::ToString for #name #ty_generics
+        #where_clause
+        {
+            fn to_string(&self) -> String {
+                match self {
+                    #variants
+                }
+            }
+        }
+    };
+    TokenStream::from(expanded)
+}

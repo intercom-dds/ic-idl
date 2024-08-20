3bd15d4349f8a8ab1d20c604e23fbe9298b6b9f3
Idar Carlsen <idar.carlsen@kongsberg.com>
2024-08-20 11:56:46 +0200

    Add idlpp as a separate executable for debugging

---
+++ tools/idlpp/src/main.rs
@@ -0,0 +1,67 @@
+// Copyright 2024 KONGSBERG
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use std::path::PathBuf;
+
+use ic_cli::Command;
+use ic_preproc::ProcArgs;
+
+/// C99-compliant preprocessor
+#[derive(Command, Default)]
+struct Options {
+    /// Add directory to include search paths
+    #[option(short = 'I', long, arg = "dir")]
+    include: Vec<PathBuf>,
+
+    /// Define preprocessor directive <def> to <val>
+    #[option(short = 'D', long, arg = "def[=val]")]
+    define: Vec<String>,
+
+    #[option(positional)]
+    files: Vec<PathBuf>,
+}
+
+fn main() {
+    let opts = Options::parse();
+    let defines = opts.define.iter().map(|v| {
+        v.split_once('=')
+            .map_or_else(|| (v.as_str(), None), |(k, v)| (k, Some(v)))
+    });
+
+    let args = ProcArgs::default()
+        .includes(opts.include)
+        .defines(defines)
+        .strip_comments(false);
+
+    for file in &opts.files {
+        let (output, errors) = ic_preproc::to_string(file, &args).unwrap();
+        for error in errors {
+            eprintln!("error: {error}");
+        }
+        println!("#line 0 {file:?}\n{output}");
+    }
+}

8ebe76881f0855abe88761fe0450a2119b4d9144
Idar Carlsen <idar.carlsen@kongsberg.com>
2024-08-31 23:56:57 +0200

    Add ic-vfs as an abstraction over filesystem

---
+++ crates/ic-vfs/src/lib.rs
@@ -0,0 +1,137 @@
+// Copyright 2024 KONGSBERG
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::io;
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
+
+use ic_alloc::arena::{Arena, Id};
+use ic_syntax::Span;
+
+/// An ID of a file in the [`SourceMap`].
+pub type FileId = Id<FileInfo>;
+
+#[derive(Debug)]
+#[must_use]
+pub struct FileInfo {
+    /// Absolute path of the file.
+    // TODO: store filename elsewhere?
+    pub path: PathBuf,
+
+    /// If we imagine that our preprocessor creates a single file where
+    /// all includes have been inlined, this span would represent the span of
+    /// the expanded include in the amalgamation.
+    pub span: Span,
+
+    /// Contents of the file.
+    pub source: Rc<str>,
+
+    /// Populated if this file was included by another file, and if so, the
+    /// span of the `#include` directive.
+    pub included_from: Option<(FileId, Span)>,
+}
+
+#[derive(Debug, Default)]
+pub struct SourceMap {
+    sources: Arena<FileInfo>,
+    files: HashMap<PathBuf, FileId>,
+}
+
+impl SourceMap {
+    /// # Errors
+    ///
+    /// Returns an error if `path` does not exist, if we do not have permission
+    /// to read the file, or if the contents of the file are not valid UTF-8.
+    pub fn open<P: AsRef<Path>>(&mut self, path: P) -> io::Result<(FileId, Rc<str>)> {
+        let path = std::path::absolute(path.as_ref())?;
+        let src = match self.files.entry(path) {
+            Entry::Occupied(id) => {
+                let id = *id.get();
+                (id, self.source(id))
+            }
+            Entry::Vacant(v) => {
+                let source = Rc::from(std::fs::read_to_string(v.key())?);
+                let path = v.key().clone();
+                let id = self.insert(path, source, Span::default());
+                (id, self.source(id))
+            }
+        };
+        Ok(src)
+    }
+
+    /// # Panics
+    ///
+    /// This may panic if the ID does not exist in the `SourceMap`. This can
+    /// only happen if you have mixed up IDs between multiple `SourceMap`
+    /// instances.
+    #[must_use]
+    pub fn file_info(&self, id: FileId) -> &FileInfo {
+        self.sources.get(id).unwrap()
+    }
+
+    #[must_use]
+    pub fn source(&self, id: FileId) -> Rc<str> {
+        self.file_info(id).source.clone()
+    }
+
+    #[must_use]
+    pub fn span_of_file(&self, id: FileId) -> Span {
+        self.file_info(id).span
+    }
+
+    /// Returns the name of the specified file.
+    #[must_use]
+    pub fn name(&self, id: FileId) -> &Path {
+        &self.file_info(id).path
+    }
+
+    /// Returns the absolute path of the specified file.
+    #[must_use]
+    pub fn path(&self, id: FileId) -> &Path {
+        &self.file_info(id).path
+    }
+
+    #[must_use]
+    pub fn line_span(&self, _id: FileId) -> Span {
+        todo!()
+    }
+
+    fn insert(&mut self, path: PathBuf, source: Rc<str>, span: Span) -> FileId {
+        let info = FileInfo {
+            path: path.clone(),
+            span,
+            source,
+            included_from: None,
+        };
+
+        let id = self.sources.alloc(info);
+        self.files.insert(path, id);
+        id
+    }
+}

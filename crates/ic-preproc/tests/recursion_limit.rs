0c03bdbb52534e94babbcdab03701dc045e39c93
Idar Carlsen <idar.carlsen@kongsberg.com>
2024-09-06 20:52:18 +0200

    Verify we hit recursion depth limit in a file with non-guarded recursive includes

---
+++ crates/ic-preproc/tests/recursion_limit.rs
@@ -0,0 +1,45 @@
+// Copyright 2024 KONGSBERG
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use ic_preproc::{with_state, ProcArgs, State};
+use ic_vfs::{Include, SourceMap};
+
+#[test]
+fn recursion_limit() {
+    let mut vfs = SourceMap::default();
+    let (id, _) = vfs
+        .open("tests/recursion_limit.idl", Include::Local)
+        .unwrap();
+
+    let args = ProcArgs::default();
+    let mut state = State::new();
+    with_state(id, args, &mut state, &mut vfs).for_each(drop);
+
+    // Verifies that we hit the recursion depth limit with a file that has no
+    // header guards. This also serves as a negative test for `#pragma once`.
+    assert_eq!(state.errors().len(), 1);
+}

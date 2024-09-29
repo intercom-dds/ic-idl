33349f9a829427d4d5d82cee31fd087d4b16dee5
Idar Carlsen <idar.carlsen@kongsberg.com>
2024-09-29 18:03:26 +0200

    Properly report name collisions from struct members, etc

--- crates/ic-hir/src/hygiene.rs
+++ crates/ic-hir/src/hygiene.rs
@@ -26,6 +26,8 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use ic_alloc::insensitive::CaseSet;
+use ic_cli::color::Colorize;
+use ic_diagnostic::{Diag, Label, error_span};
 
 use crate::hir::{
     BitmaskTy, Def, DefId, DefKind, EnumTy, ExceptTy, Ident, InterfaceTy, ProtoTy, StructTy,
@@ -36,18 +38,23 @@ use crate::{Context, DefIter};
 
 struct Hygiene<'a> {
     ctx: &'a Context,
+    errors: &'a mut Vec<Diag>,
 }
 
 impl Hygiene<'_> {
-    fn check_def<'a>(elems: impl Iterator<Item = &'a Ident>) {
+    fn check_def<'a>(&mut self, elems: impl Iterator<Item = &'a Ident>) {
         let mut seen = CaseSet::default();
-        Self::with_set(&mut seen, elems)
+        self.with_set(&mut seen, elems)
     }
 
-    fn with_set<'a>(seen: &mut CaseSet<'a>, elems: impl Iterator<Item = &'a Ident>) {
+    fn with_set<'a>(&mut self, seen: &mut CaseSet<'a>, elems: impl Iterator<Item = &'a Ident>) {
         for ident in elems {
             if !seen.insert(ident.name.as_str()) {
-                tracing::error!("collision occurred: {}", ident.name);
+                let diag = error_span(
+                    format!("duplicate registration of name `{}`", ident.name.yellow()),
+                    Label::new(ident.span).message("redefined here"),
+                );
+                self.errors.push(diag);
             }
         }
     }
@@ -61,46 +68,47 @@ impl<'a> Visitor<'a> for Hygiene<'_> {
         let mut parent_id = data.parent;
         while let Some(parent) = parent_id {
             let parent = self.ctx.type_of(parent);
+
             // TODO: move this to a separate function and re-use for
             // valuetype/interfaces.
             match &parent.kind {
                 DefKind::Struct(v) => {
-                    Self::with_set(&mut seen, v.members.iter().map(|v| &v.ident));
+                    self.with_set(&mut seen, v.members.iter().map(|v| &v.ident));
                     parent_id = v.parent;
                 }
                 _ => (),
             }
         }
 
-        Self::with_set(&mut seen, data.members.iter().map(|v| &v.ident));
+        self.with_set(&mut seen, data.members.iter().map(|v| &v.ident));
     }
 
     fn visit_except(&mut self, def: &'a Def, data: &'a ExceptTy) {
-        Self::check_def(data.members.iter().map(|v| &v.ident));
+        self.check_def(data.members.iter().map(|v| &v.ident));
     }
 
     fn visit_enum(&mut self, def: &'a Def, data: &'a EnumTy) {
-        Self::check_def(data.fields.iter().map(|v| &v.ident));
+        self.check_def(data.fields.iter().map(|v| &v.ident));
     }
 
     fn visit_bitmask(&mut self, def: &'a Def, data: &'a BitmaskTy) {
-        Self::check_def(data.flags.iter().map(|v| &v.ident));
+        self.check_def(data.flags.iter().map(|v| &v.ident));
     }
 
     fn visit_union(&mut self, def: &'a Def, data: &'a UnionTy) {
         // TODO: check for duplicated case labels too?
-        Self::check_def(data.variants.iter().map(|v| &v.ident));
+        self.check_def(data.variants.iter().map(|v| &v.ident));
     }
 
     fn visit_interface(&mut self, def: &'a Def, data: &'a InterfaceTy) {
         // TODO: attributes and nested items. continue traversing nested defs.
-        Self::check_def(data.prototypes.iter().map(|v| &v.ident));
+        self.check_def(data.prototypes.iter().map(|v| &v.ident));
     }
 }
 
-pub fn check(ctx: &Context, order: &[DefId]) {
+pub fn check(ctx: &Context, order: &[DefId], errors: &mut Vec<Diag>) {
     let iter = DefIter::with_order(ctx, order);
-    let mut hygiene = Hygiene { ctx };
+    let mut hygiene = Hygiene { ctx, errors };
     for def in iter {
         walk_def(&mut hygiene, def);
     }

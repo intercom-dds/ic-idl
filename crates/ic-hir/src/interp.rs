2d57cdfc718dcc86c6d301c7ee39f37dd86e534e
Idar Carlsen <idar.carlsen@kongsberg.com>
2025-04-05 15:09:11 +0200

    Eval complex init expression lists

--- crates/ic-hir/src/interp.rs
+++ crates/ic-hir/src/interp.rs
@@ -158,7 +158,12 @@ impl Interp<'_> {
             Expr::Path(v) => Numeric::Const(DefId::_do_not_use()),
             Expr::Unary(v) => Numeric::Int64(self.eval_unary(v)),
             Expr::Binary(v) => Numeric::Int64(self.eval_binary(v)),
-            Expr::InitList(v) => todo!(),
+            Expr::InitList(v) => Numeric::InitList(
+                v.values
+                    .iter()
+                    .map(|v| self.eval_expr_ty(&v.value))
+                    .collect(),
+            ),
         }
     }
 

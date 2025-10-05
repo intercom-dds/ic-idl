c59fde5abb8f082bbca152f711b30eb55c1ae1ec
Idar Carlsen <idar.carlsen@kongsberg.com>
2025-10-05 20:44:16 +0200

    Missing #pragma once in memory.h

--- library/cpp/defs/ic_cts/memory.h
+++ library/cpp/defs/ic_cts/memory.h
@@ -1,4 +1,4 @@
-// Copyright 2024 KONGSBERG
+// Copyright 2025 KONGSBERG
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -24,6 +24,8 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
 
 #include <utility>
 

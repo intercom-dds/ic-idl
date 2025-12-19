ccd37d2ae20e51945a9129747db617ac28bb7901
Idar Carlsen <idar.carlsen@kongsberg.com>
2025-12-19 01:35:20 +0100

    Update keyword list

--- crates/ic-codegen-cpp/src/lib.rs
+++ crates/ic-codegen-cpp/src/lib.rs
@@ -42,14 +42,15 @@ const KEYWORDS: &[&str] = &[
     "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor", "bool", "break",
     "case", "catch", "char", "char16_t", "char32_t", "char8_t", "class", "co_await", "co_return",
     "co_yield", "compl", "concept", "const", "const_cast", "consteval", "constexpr", "constinit",
-    "continue", "decltype", "default", "delete", "do", "double", "dynamic_cast", "else", "enum",
-    "explicit", "export", "extern", "false", "float", "for", "friend", "goto", "if", "import",
-    "inline", "inspect", "int", "long", "module", "mutable", "namespace", "new", "noexcept", "not",
-    "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected", "public", "register",
-    "reinterpret_cast", "really", "requires", "return", "short", "signed", "sizeof", "static",
+    "continue", "contract_assert", "decltype", "default", "delete", "do", "double", "dynamic_cast",
+    "else", "enum", "explicit", "export", "extern", "false", "float", "for", "friend", "goto",
+    "if", "import", "inline", "int", "long", "module", "mutable", "namespace", "new", "noexcept",
+    "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private", "protected", "public",
+    "register", "reinterpret_cast", "requires", "return", "short", "signed", "sizeof", "static",
     "static_assert", "static_cast", "struct", "switch", "template", "this", "thread_local",
-    "throw", "throws", "true", "try", "typedef", "typeid", "typename", "union", "unsigned",
-    "using", "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+    "throw", "true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using",
+    "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+
     // not keywords(?), operators, "identifiers with meaning", etc
     "typeof", "assert",
 ];

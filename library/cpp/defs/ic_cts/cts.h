a214aca1cde363abb6f516941837bf21ea407d91
Idar Carlsen <idar.carlsen@kongsberg.com>
2025-12-20 14:58:56 +0100

    Fix C++ warnings in headers

--- library/cpp/defs/ic_cts/cts.h
+++ library/cpp/defs/ic_cts/cts.h
@@ -25,6 +25,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#pragma once
+
 // IWYU pragma: begin_exports
 #include <cstdint>
 #include <functional>

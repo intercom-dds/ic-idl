acc0ca10ed1a9b5b1fbd09bc9ae9d368d5a863a5
Idar Carlsen <idar.carlsen@kongsberg.com>
2026-01-16 16:09:00 +0100

    Suppress clippy warning

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -25,7 +25,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#![allow(clippy::print_stdout, clippy::print_stderr)]
+#![allow(clippy::print_stdout, clippy::print_stderr, clippy::large_enum_variant)]
 
 use ic_cli::Command;
 
